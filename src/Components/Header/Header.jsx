b24de8abe1c9151ac1683feb849216db7cd1cd5a
Glenn Niblett <glenn.niblett@hotmail.co.uk>
2026-06-30 18:13:22 +0100

    Added header component with retro styling

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+function Header () {
+    return (
+        <header className="flex flex-col p-12 justify-center ">
+            <div className="self-center justify-center p-4 px-12 border-[#3D3D3D] border-4 bg-[#595959]">
+                <h1 className="text-center text-4xl text-[#80FF10]">
+                Vaulted Games
+                </h1>
+                </div>
+            <p className="mt-6 text-center text-xl text-white">Welcome to 'Vaulted Games'! <br/>The number 1 place to track organize and store your history of video games that you've played - at any time, on any device, throughout your own gaming adventures!</p>
+        </header> 
+    )
+}
+
+export default Header;
