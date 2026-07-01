34ea3618758deefde0b57f6acbae471d0ad9aa0d
Glenn Niblett <glenn.niblett@hotmail.co.uk>
2026-07-01 12:11:55 +0100

    Added gameData.json file with mock data and genres.js filled with genres and sub-genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
 
+const GameData = [
+  {
+    id: 1,
+    platform: 'Nintendo 64',
+    year: 1998,
+    title: 'The Legend of Zelda: Ocarina of Time',
+    genre: 'Action-adventure',
+    rating: 'Top 10',
+    rank: 5
+  },
+]
 
 function App() {
  return (
---
+++ src/Data/genres.js
@@ -0,0 +1,19 @@
+const genres = {
+  
+    Action: ['Battle Royale', 'Beat-em up','Fighting', 'Open World FPS', 'Platformer', 'Stealth', 'Survival'],
+    Adventure: ['Action-adventure', 'Metroidvania', 'Roguelike', 'Geolocation-based'],
+    Casual: ['Auto Battle', 'Board Game', 'Cosy-farm simulation', 'Creative', 'Educational','Idle', 'Music-rhythm', 'Party', 'Puzzle', 'Trivia'],
+    Card: ['Collectible Card Game', 'Deck-building', 'Gambling/casino', 'Playing cards'],
+    Horror: ['Action-Horror', 'Mystery-Horror', 'Psychological Horror', 'Survival-Horror'],
+    Racing: ['Arcade racing', 'Simulation racing'],
+    RPG: ['Action RPG', 'Hack and slash', 'JRPG', 'MMORPG', 'Open World RPG', 'Sandbox RPG', 'Tactical RPG'],
+    SciFi: ['Cyberpunk', 'Space'],
+    Shooter: ['Bullet Hell', 'Extraction', 'First-person shooter', 'Third-person shooter'],
+    Simulation: ['City Builder', 'Dating Sim', 'God Game', 'Life simulation', 'Tycoon'],
+    StoryRich: ['Point-and-click', 'Romance', 'Text Adventure', 'Visual novel'],
+    Strategy: ['Artillery', 'Grand Strategy', 'MOBA', 'RTT', 'RTS', 'Turn-based strategy', 'Tactical', 'Tower Defense', 'War Game'],
+    Sports: ['Arcade sports', 'Simulation sports'],
+    Misc: ['AI Generated', 'Augmented Reality', 'CRPG', 'Interactive movie', 'Mobile', 'NFT-Blockchain', 'Sandbox','Virtual Reality']
+},
+
+export default genres
